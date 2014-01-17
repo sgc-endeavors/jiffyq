f633585fc0775cf2a9caac5e15e56e4fc5e35e11
Seth Geyer <seth.geyer@gmail.com>
2014-01-17 16:15:38 -0700

    Killed keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,8 +12,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'AKIAJADPJEW26KLP3UQA',       # required
-    :aws_secret_access_key  => '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',       # required
+    :aws_access_key_id      => '',       # required
+    :aws_secret_access_key  => '',       # required
     :region                 => 'us-west-2'  # required, defaults to 'us-east-1'
     
   }
