fb451eaa2723914ffccbc0019f87e63a328018d6
Seth Geyer <seth.geyer@gmail.com>
2014-01-22 14:55:53 -0700

    t'shoot heroku s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,10 @@
 
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',       # required
-   # :aws_access_key_id      => 'ACCESS_KEY',       # required
-   # :aws_secret_access_key  => 'SECRET_KEY'#,       # required
-    #:region                 => 'eu-west-1'#'us-west-2'  # required, defaults to 'us-east-1'
-    
+   provider:               "AWS",       # required
+   aws_acces_key_id: ENV["AWS_ACCESS_KEY_ID"],
+   aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]  
   }
-  config.fog_directory  = 'o-meter-img'
+  config.fog_directory  = ENV["S3_bucket_name"]
 
 end
