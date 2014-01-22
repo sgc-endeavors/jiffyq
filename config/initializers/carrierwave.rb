aa7545749a7f026f38189249a3958dd2fbf03b20
Seth Geyer <seth.geyer@gmail.com>
2014-01-22 14:37:29 -0700

    troubleshooting s3 on heroku

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',       # required
+    :aws_access_key_id      => 'ACCESS_KEY',       # required
+    :aws_secret_access_key  => 'SECRET_KEY'#,       # required
+    #:region                 => 'eu-west-1'#'us-west-2'  # required, defaults to 'us-east-1'
+    
+  }
+  config.fog_directory  = 'o-meter-img'
+
+end
