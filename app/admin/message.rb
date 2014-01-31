c1104555627f88615d685d9e55c280bd4464621d
Seth Geyer <seth.geyer@gmail.com>
2014-01-31 15:54:30 -0700

    resolved issue associated w/ activeadmin not being OK w/ the message "identifier" being parasitized (for the id)

--- app/admin/message.rb
+++ app/admin/message.rb
@@ -1,3 +1,42 @@
 ActiveAdmin.register Message do
 
+	index do
+		selectable_column
+		column :id
+		column :question
+		column :button1
+		column :button2
+		column :response1
+		column :response2
+		column :identifier
+		column :status
+		column :page_views
+		column :image_id
+		column :user_id
+		column :origin_message
+		column :updated_at
+		default_actions
+
+
+	end
+
+
+
+
+
+
+	controller do
+		def show
+			@message = Message.where(identifier: params[:id]).first
+		end
+		def edit
+			@message = Message.where(identifier: params[:id]).first
+		end
+		def destroy
+			@message = Message.where(identifier: params[:id]).first
+			@message.destroy
+			redirect_to admin_messages_path
+		end
+	end
+
 end
